e263db3e3ab8fdf8d70d975a093b5d1fbe557984
ahbakken <ahbakken@uio.no>
2022-09-05 00:32:52 +0200

    Ukesoppgaver36: feat - find_min_element

--- ukesoppgaver36.py
+++ ukesoppgaver36.py
@@ -38,15 +38,30 @@ class Node:
         return None
       return self.rightChild.find_node_value(x)
 
+# find smallest element
+  def find_min_element(self):
+    if self == None:
+      return self
+    current = self
+    while current.leftChild is not None:
+      current = current.leftChild 
+    return current
+  
+
+
+# delete from binary tree
 
 # Initialize class, insert new nodes to the tree
 root = Node(15)
 root.set_next_node(2)
 root.set_next_node(6)
 root.set_next_node(23)
-root.set_next_node(44)
-root.set_next_node(12)
+root.set_next_node(1)
+root.set_next_node(12) 
+root.set_next_node(-23)                            
 
 print("Root element value: ", root.element)
 
 print("Find node with value x: ", (root.find_node_value(144)))
+
+print(root.find_min_element().element)
