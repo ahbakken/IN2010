2e0ecf1f43c95e36f66c2ef17829c3c144b32dc6
ahbakken <ahbakken@uio.no>
2022-09-18 20:30:27 +0200

    implementation: sort, selection sort

--- imp-sort.py
+++ imp-sort.py
@@ -1,12 +1,14 @@
-#bubble sort
+#sorting
 #input: array with n elements
 #output: sortet array with the same n elements
+
+#bubble sort
 def bubbleSort(array):
     n = len(array)
     i = 0
-    while (i < n-1):
+    while (i < n-1): #stop at n-2
         j = 0
-        while (j < n-i-1):
+        while (j < n-i-1): #stop at n-2
             if array[j] > array[j+1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
             j+=1
@@ -14,6 +16,29 @@ def bubbleSort(array):
     return array
 
 inputList = [1, 5, 12, 34, 54, 42, 33, 65, 56, 555, 64, 13 ,47 , 0]
+print("Before bubble sort", inputList)
+print("After  bubble sort", bubbleSort(inputList))
+print("__________________________________________________________")
 
-bubbleSort(inputList)
-print(bubbleSort(inputList))
+#selection sort
+def selectionSort(array):
+    n = len(array)
+    i = 0
+    while (i < n-1): #iterate throught the array, stop after n-2
+        k = i
+        j = i+1
+        while j < n:  #iterate through the array again, stop at n-1
+            if array[j] < array[k]: #check if element after is bigger than element before
+                k = j #change value of k to the smallest element in the array
+            j+=1
+        if i != k:  
+            array[i], array[k] = array[k], array[i] #swap place for i and the smallest element after i
+        i+=1
+        print(array)
+    return array
+
+inputList = [1, 5, 12, 34, 54, 42, 33, 65, 56, 555, 64, 13 ,47 , 0]
+print("Before selection sort", inputList)
+print("After  selection sort", selectionSort(inputList))
+print("__________________________________________________________")
+
