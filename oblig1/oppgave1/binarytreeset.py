26faf865f7d2b1875048844fd7f98d2050191a9a
ahbakken <ahbakken@uio.no>
2022-09-22 21:59:02 +0200

    oblig1: oppgave1 ferdig, test-filer, start oppg2

--- oblig1/oppgave1/binarytreeset.py
+++ oblig1/oppgave1/binarytreeset.py
@@ -4,7 +4,7 @@
 # remove(set, x) fjerner x fra mengden
 # size(set) gir antall elementer i mengden
 
-from fileinput import filename
+import fileinput
 
 #Lager noder for treet
 class Node:
@@ -12,6 +12,7 @@ class Node:
     self.val = value
     self.left = None
     self.right = None
+    self.size = 0
 
     
 # setter x inn i binaertreet or returnerer noden med x
@@ -30,77 +31,87 @@ def insert(root, x):
 def contains(root, x):
     if root is None:
         return False
-    if root.val is x:
+    if root.val == x:
         return True
     if x < root.val:
         return contains(root.left, x)
-    if x > root.val:
-        return contains(root.right, x)
 
-#hjelpeprosedyre, finner minste verdi i root
-#skal finnes lengst til venstre i treet. 
-def findMin(root):
-    if root.left is not None:
-        return findMin(root.left)
-    else: 
-        return root
+    return contains(root.right, x)
 
 #fjerner en node fra treet basert paa verdien som blir sendt inn. 
 def remove(root, x):
-    
     if root is None: 
         return root
     #om x er minde en rotverdien
     #fortsetter i venstre subtre
     if x < root.val:
         root.left = remove(root.left, x)
+        return root
     #om x er storre en rotverdien
     #fortsetter i hoyre subtre
     elif x > root.val:
         root.right = remove(root.right, x)
+        return root
+    #if the root is a leaf node we make it None
+    if root.left is None and root.right is None:
+          return None
     #x er verken storre eller mindre, maa vaere lik rotverdien
-    else: 
-        #sjekker om rot har barn venstre
-        if root.left is None:
-            temp = root.right #midlertidig holder for barnenoden
-            root = temp #dette er noden vi vil ha slettet
-            return root #kobler sammen barn of bestemor til root-barn
-        elif root.right is None:
-            temp = root.left
-            root = temp
-            #erstatte temp paa en anen maate
-            return root #barn returneres og tar plass til forelder
-        #har to barn
-        temp = findMin(root.right)
-        root.val = temp.val
-        root.right = remove(root.right, temp.val)
-        print(root.val)
+    
+    #sjekker om rot har barn venstre
+    if root.left is None:
+        temp = root.right #midlertidig holder for barnenoden
+        root = None #dette er noden vi vil ha slettet
+        return root #kobler sammen barn of bestemor til root-barn
+    elif root.right is None:
+        temp = root.left
+        root = None
+        return temp #barn returneres og tar plass til forelder
+    #har to barn
+    tempParent = root
+    temp = root.right
+
+    while temp.left != None:
+        tempParent = temp
+        temp = temp.left
+    
+    if tempParent != root:
+        tempParent.left = temp.right
+    else:
+        tempParent.right = temp.right
+
+    root.val = temp.val
+    # print(root.val)
     return root
 
+def size(root, x):
+    while root != None:
+        print(size)
+        x += 1
+        if root.left != None:
+            size(root.left, x)
+        if root.right != None:
+            size(self.right)
+    return x
 
 #lager tom rotnode, uten innhold
 root = None
 sizeTree = 0
 
 # opner fil som stdin
-filename = input("Enter filname: ")
-f = open(filename, 'r')
-i = int(f.readline())
-x = 0
-
-# itererer gjennom fil for aa utfore oppgaver.
-while x < i:
-    line = f.readline()
+for line in fileinput.input():
     if line.startswith("insert"):
         tall = line.split(' ')
         verdi = int(tall[-1])
     # setter inn roten forst
         if (root == None): 
-           root = insert(root, verdi)
-    #om root har en verdi vil insert brukes
-        else:
             root = insert(root, verdi)
             sizeTree+=1
+            
+    #om root har en verdi vil insert brukes
+        else:
+            if not contains(root,verdi):
+                root = insert(root, verdi)
+                sizeTree+=1
     elif line.startswith("contains"):
         tall = line.split(' ')
         verdi = int(tall[-1])
@@ -111,9 +122,11 @@ while x < i:
         tall = line.split(' ')
         verdi = int(tall[-1])
         if contains(root, verdi):
-            root = remove(root, verdi)
+            root = remove(root, verdi) 
             sizeTree-=1
+            
+    #siden storrelsen paa treet blir oppdatert for hver remove og inser, vil 
     elif line.startswith("size"):
         print(sizeTree)
-    x+=1
-f.close()
+
+# itererer gjennom fil for aa utfore oppgaver.
