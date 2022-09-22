798152f97d715df3a4ba5b3fb607c7514cc931b2
ahbakken <ahbakken@uio.no>
2022-09-22 02:34:33 +0200

    oblig1: oppgave 1 finish, remove fix

--- oblig1/effektivemengder.py
+++ oblig1/effektivemengder.py
@@ -8,29 +8,29 @@ from fileinput import filename
 
 #Lager noder for treet
 class Node:
-  def __init__(self, value, height):
+  def __init__(self, value):
     self.val = value
     self.left = None
     self.right = None
-    self.height = height
+
     
 # setter x inn i binaertreet or returnerer noden med x
 # antar at treet er balansert ved insert, som gir: O(log(n))
-def insert(root, x, h):
-    if root == None or root.val == None:
-        root = Node(x, h)
-    elif (root.val < x):
-        root.right = insert(root.right, x, h+1)
-    else:
-        root.left = insert(root.left, x, h+1)
+def insert(root, x):
+    if root is None:
+        return Node(x)
+    elif (x < root.val):
+        root.left = insert(root.left, x)  
+    elif (x > root.val):
+        root.right = insert(root.right, x)
     return root
 
 # sjekker om x er i treet 
 # har samme kompleksitet som insert: O(log(n))
 def contains(root, x):
-    if root == None or root.val == None:
+    if root is None:
         return False
-    if root.val == x:
+    if root.val is x:
         return True
     if x < root.val:
         return contains(root.left, x)
@@ -40,42 +40,53 @@ def contains(root, x):
 #hjelpeprosedyre, finner minste verdi i root
 #skal finnes lengst til venstre i treet. 
 def findMin(root):
-    if root == None or root.val == None:
-        return root
-    if root.left != None:
+    if root.left is not None:
         return findMin(root.left)
-    else: return root
+    else: 
+        return root
 
 #fjerner en node fra treet basert paa verdien som blir sendt inn. 
 def remove(root, x):
-    if root == None or root.val == None:
-        return None
+    
+    if root is None: 
+        return root
+    #om x er minde en rotverdien
+    #fortsetter i venstre subtre
     if x < root.val:
         root.left = remove(root.left, x)
-        return root
-    if x > root.val:
+    #om x er storre en rotverdien
+    #fortsetter i hoyre subtre
+    elif x > root.val:
         root.right = remove(root.right, x)
-        return root
-    if root.left == None:
-        return root.right
-    if root.right == None:
-        return root.right
-    holder = findMin(root.right)
-    root.val = holder.val
-    root.right = remove(root.right, holder.val)
+    #x er verken storre eller mindre, maa vaere lik rotverdien
+    else: 
+        #sjekker om rot har barn venstre
+        if root.left is None:
+            temp = root.right #midlertidig holder for barnenoden
+            root = temp #dette er noden vi vil ha slettet
+            return root #kobler sammen barn of bestemor til root-barn
+        elif root.right is None:
+            temp = root.left
+            root = temp
+            #erstatte temp paa en anen maate
+            return root #barn returneres og tar plass til forelder
+        #har to barn
+        temp = findMin(root.right)
+        root.val = temp.val
+        root.right = remove(root.right, temp.val)
+        print("REMOVE root value", root.val)
     return root
 
-#lager tom rotnode, uten innhold, hoyde -1
+
+#lager tom rotnode, uten innhold
 root = None
+sizeTree = 0
 
 # opner fil som stdin
 filename = input("Enter filname: ")
 f = open(filename, 'r')
 i = int(f.readline())
 x = 0
-liste = [root]
-#oppdateres for hver gang en ny node legges til eller trekkes fra 
-sizeList = len(liste)
 
 # itererer gjennom fil for aa utfore oppgaver.
 while x < i:
@@ -85,12 +96,11 @@ while x < i:
         verdi = int(tall[-1])
     # setter inn roten forst
         if (root == None): 
-            root = Node(verdi, 0)
+           root = insert(root, verdi)
     #om root har en verdi vil insert brukes
         else:
-            if (not contains(root, verdi)):
-                liste.append(insert(root, verdi, 0))
-                sizeList = len(liste)
+            root = insert(root, verdi)
+            sizeTree+=1
     elif line.startswith("contains"):
         tall = line.split(' ')
         verdi = int(tall[-1])
@@ -101,11 +111,9 @@ while x < i:
         tall = line.split(' ')
         verdi = int(tall[-1])
         if contains(root, verdi):
-            rem = remove(root, verdi)
-            if (rem != None):
-                liste.remove(rem)
-                sizeList = len(liste)
+            root = remove(root, verdi)
+            sizeTree-=1
     elif line.startswith("size"):
-        print(sizeList)
+        print(sizeTree)
     x+=1
 f.close()
