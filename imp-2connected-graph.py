0277e9284839ea343a2762f7a57ed9c260aecce7
ahbakken <ahbakken@uio.no>
2022-11-19 23:18:35 +0100

    IN2010: imp 2connected, reverse a graph algorithm

--- imp-2connected-graph.py
+++ imp-2connected-graph.py
@@ -48,9 +48,6 @@ gdict2 = {'a': ['b', 'c'], 'b': ['c', 'a'], 'c': ['b', 'd'], 'd': ['c', 'e'], 'e
 # ---------------------------------------------------------------
 
 # Finding separation nodes
-
-
-
 def separationVertices(graph):
     s = list(graph)[0] #choosing first node
     s_edges = graph[s]
@@ -76,13 +73,10 @@ def separationVerticesRec(graph, u, d):
         if v in depth:
             low[u] = min(low[u], depth[v])
             continue
-            
-        
         separationVerticesRec(graph, v, d+1)
         low[u] = min(low[u], low[v])
         if d <= low[v]:
             seps.add(u)
-
 
 #this is 2-connected
 # depth = dict()
@@ -97,30 +91,54 @@ def separationVerticesRec(graph, u, d):
 # seps = set()
 # print('The graph2 separation nodes: ', separationVertices(gdict2))
 
-
 #use separationVertices to test is a graph is 2-connected
 def isBiConnected(graph):
     return len(separationVertices(graph)) < 1
 
-depth = dict()
-low = dict()
-seps = set()
-print('Graph1 is 2-connected test with separation: ', isBiConnected(gdict1))
+# depth = dict()
+# low = dict()
+# seps = set()
+# print('Graph1 is 2-connected test with separation: ', isBiConnected(gdict1))
 
-depth = dict()
-low = dict()
-seps = set()
-print('Graph2 is 2-connected test with separation: ', isBiConnected(gdict2))
+# depth = dict()
+# low = dict()
+# seps = set()
+# print('Graph2 is 2-connected test with separation: ', isBiConnected(gdict2))
+
+
+# reverse a graph
+def reverseGraph(graph):
+    graph_inv = defaultdict(list)
+    #make new dict that is reversed
+    for node, edges in graph.items():
+        for edge in edges:
+            graph_inv[edge].append(node)
+    #add any missing nodes
+    for node in graph.keys():
+        if node not in graph_inv.keys():
+            graph_inv[node] = []
+    #transform to common dict
+    return dict(graph_inv)
+
+#2connected graph
+gdict1 = {'a': ['b', 'e'], 'b': ['c', 'a', 'e'], 'c': ['b', 'd'], 'd': ['c', 'e'], 'e':['d', 'b', 'a']}
+#separation nodes
+gdict2 = {'a': ['b', 'c'], 'b': ['c', 'a'], 'c': ['b', 'd'], 'd': ['c', 'e'], 'e':['d']}
+#clear directed graph
+gdict3 = {'a': ['b'], 'b': ['d'], 'c': ['b'], 'd': ['c', 'e'], 'e':[]}
+
+print(reverseGraph(gdict2))
+
+
 
 # Graph class, might be used at some point...
 # class Graph:
-#     count = 0
  
 #     def __init__(self, vertices):
 #         self.V = vertices  # No. of vertices
 #         self.graph = defaultdict(list)  # default dictionary to store graph
-#         self.Time = 0
  
+
 #     # function to add an edge to graph
 #     def addEdge(self, u, v):
 #         self.graph[u].append(v)
